8d8527724422e56164be05e50d5f23027b39c936
edem <unreshablekhon@gmail.com>
2023-05-12 15:55:17 +0000

    terms and conditions page setup

---
+++ pages/terms-and-conditions.js
@@ -0,0 +1,30 @@
+   import React from 'react';
+
+   const Terms = () => {
+     return (
+       <div className='w-full h-full'>
+         <h1>Terms and Conditions</h1>
+         <p>Here are the terms and conditions of our service:</p>
+         <ul>
+           <li>Maintenance and Support: The company will provide maintenance and support services for the software during the term of this agreement.</li>
+           <li>Ownership: The software and all intellectual property rights are owned by the company.</li>
+           <li>Restrictions: The customer shall not copy, modify, distribute, or create derivative works of the software.</li>
+           <li>Confidentiality: The customer shall maintain the confidentiality of the software and not disclose any information regarding the software to any third party.</li>
+            <li>Termination: The company may terminate this agreement if the customer breaches any of the terms and conditions.</li>
+            <li>Warranty: The company warrants that the software will operate in accordance with the documentation.</li>
+            <li>Limitation of Liability: The company shall not be liable for any indirect, incidental, or consequential damages arising out of the use of the software.</li>
+         </ul>
+       </div>
+     );
+   };
+
+   export default Terms;
+
+   
+
+
+
+
+
+
+
