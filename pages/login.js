8d9b9e67df99d8258ed847ce6c4a836137eade45
edem <unreshablekhon@gmail.com>
2023-05-19 05:09:59 +0000

    created the login page

--- pages/login.js
+++ pages/login.js
@@ -1,14 +1,145 @@
-import React from "react"
+import React, { useState } from "react";
+
+import Logo01 from "../public/icons/loginPage/loginicon01";
+import Link from "next/link";
+
+function PasswordInput() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div>
+      <label htmlFor="password"></label>
+      <input
+        type={showPassword ? "text" : "password"}
+        id="password"
+        value={password}
+        onChange={handlePasswordChange}
+      />
+      <button
+        className="text-xs font-medium mb-2"
+        onClick={togglePasswordVisibility}
+      >
+        &nbsp;&nbsp;&nbsp; {showPassword ? "Hide" : "Show"} Password
+      </button>
+    </div>
+  );
+}
 
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-    return (
-        <div className="flex flex-col relative bg-white ">
-            {/* here's the diagonal object giving at the top */}
-            <div
-                className="z-0 absolute -top-56 bottom-96 h-[500px] inset-0 bg-[#7622FF] opacity-50 transform -skew-y-[10deg]">
-            </div>
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Form validation
+    if (!email || !password) {
+      setError("Please fill out all fields.");
+      return;
+    }
+
+    try {
+      // Simulating authentication API request
+      const response = await loginAPI(email, password);
+
+      // Check authentication response
+      if (response.success) {
+        // Redirect to dashboard or update state for successful login
+        // For example:
+        // setLoggedIn(true);
+        // history.push("/dashboard");
+      } else {
+        // Handle authentication failure
+        setError(response.message);
+      }
+    } catch (error) {
+      // Handle API request errors
+      setError("An error occurred. Please try again later.");
+    }
+  };
+
+  const loginAPI = (email, password) => {
+    // Simulating authentication API request
+    return new Promise((resolve, reject) => {
+      // Simulating server response
+      setTimeout(() => {
+        if (email === "example@example.com" && password === "password") {
+          resolve({ success: true, message: "Login successful." });
+        } else {
+          resolve({ success: false, message: "Invalid credentials." });
+        }
+      }, 1000);
+    });
+  };
+
+  return (
+    <div className="bg-[#C4D7F8] h-screen">
+      <div className="flex flex-row pt-5">
+        <div className="basis-1/5 z-50 text-center">
+          <a className="text-lg font-semibold text-white">Essential</a>
+          <a className="text-xl font-semibold text-[#022568]">ERP</a>
         </div>
-    )
+      </div>
 
+      <div className="flex flex-row justify-center pt-16">
+        <Logo01 />
+      </div>
+
+      <div className="flex flex-row justify-center text-center pt-5 text-sm">
+        <div className="flex flex-col">
+          <div>Login with Company Email</div>
+        </div>
+      </div>
+
+      <div className="flex flex-row justify-center">
+        <form onSubmit={handleSubmit} className="w-full max-w-sm p-6">
+          <div className="mb-4">
+            <label htmlFor="email" className="text-xs font-medium mb-2">
+              Company Email:
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="w-full px-3 py-1 border border-gray-400 rounded"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label htmlFor="password" className="text-xs font-medium mb-2">
+              Password:
+            </label>
+            <PasswordInput />
+          </div>
+          {error && <div>{error}</div>}
+
+          <div className="flex flex-row justify-center pt-5">
+            <button
+              type="submit"
+              className="px-4 py-2 bg-indigo-500 text-white rounded hover:bg-blue-600"
+            >
+              Login
+            </button>
+          </div>
+          <div className="flex flex-row justify-center pt-5">
+            Don't have an account? &nbsp;{" "}
+            <Link classname="text-[#6C63FF] underline" href="/signup">
+              Sign Up!
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
--- pages/signup.js
+++ pages/signup.js
@@ -1,155 +1,181 @@
 import React from "react";
-import { useState } from 'react'
+import { useState } from "react";
 import Link from "next/link";
 
 import Logo01 from "../public/icons/signupPage/signupicon01";
 
 export default function SignUpPage() {
-    const [com_name, setCompanyName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [status, setStatus] = useState('');
-    const [confirm_password, setConfirmPassword] = useState('');
-    const [error, setError] = useState(null);
+  const [com_name, setCompanyName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [status, setStatus] = useState("");
+  const [confirm_password, setConfirmPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [isChecked, setIsChecked] = useState(false);
 
-    if (status === 'success') {
-        console.log("Hello Motherfucker");
+  const handleCheckboxChange = (event) => {
+    setIsChecked(event.target.checked);
+  };
+
+  if (status === "success") {
+    console.log("Hello Motherfucker");
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    // setStatus('submitting');
+    // try {
+    //     await submitForm(email);
+    //     setStatus('success');
+    // } catch (error) {
+    //     setStatus('typing');
+    //     setError(error);
+    // }
+
+    if (!com_name || !email || !password || !confirm_password || !isChecked) {
+      setError("Please fill out all fields.");
+      return;
     }
 
-    async function handleSubmit(event) {
-        event.preventDefault();
-        // setStatus('submitting');
-        // try {
-        //     await submitForm(email);
-        //     setStatus('success');
-        // } catch (error) {
-        //     setStatus('typing');
-        //     setError(error);
-        // }
-
-
-        if (!com_name || !email || !password || !confirm_password) {
-            setError("Please fill out all fields.");
-            return;
-        }
-
-        if (password !== confirm_password) {
-            setError("Passwords do not match.");
-            return;
-        }
-
-        // continue with account creation
-
+    if (password !== confirm_password) {
+      setError("Passwords do not match.");
+      return;
     }
 
-    return (
-        <div className="bg-[#C4D7F8] h-screen">
+    // continue with account creation
+  }
 
-            <div className="flex flex-row pt-5">
-                <div className='basis-1/5 z-50 text-center'>
-                    <a className='text-lg font-semibold text-white'>Essential</a>
-                    <a className='text-xl font-semibold text-[#022568]'>ERP</a>
-                </div>
-            </div>
-
-            <div className="flex flex-row justify-center pt-16">
-                <Logo01 />
-            </div>
-
-            <div className="flex flex-row justify-center text-center pt-5 text-sm">
-                <div className="flex flex-col">
-                    <div>
-                        Create an account with email
-                    </div>
-                    <div>
-                        Already have an account? Login
-                    </div>
-                </div>
-            </div>
-
-            <div className="flex flex-row justify-center">
-
-                <form onSubmit={handleSubmit} className="w-full max-w-sm p-6">
-                    <div className="mb-4">
-                        <label htmlFor="text" className="text-xs font-medium mb-2">Company Name:</label>
-                        <input
-                            type="text"
-                            id="com_name"
-                            className="w-full px-3 py-1 border border-gray-400 rounded"
-                            value={com_name}
-                            onChange={(event) => setCompanyName(event.target.value)}
-                            required
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="email" className="text-xs font-medium mb-2">Company Email:</label>
-                        <input
-                            type="email"
-                            id="email"
-                            className="w-full px-3 py-1 border border-gray-400 rounded"
-                            value={email}
-                            onChange={(event) => setEmail(event.target.value)}
-                            required
-                        />
-                    </div>
-                    <div className="mb-6">
-                        <label htmlFor="password" className="text-xs font-medium mb-2">Password:</label>
-                        <input
-                            type="password"
-                            id="password"
-                            className="w-full px-3 py-1 border border-gray-400 rounded"
-                            value={password}
-                            onChange={(event) => setPassword(event.target.value)}
-                            required
-                        />
-                    </div>
-                    <div className="mb-6">
-                        <label htmlFor="password" className="text-xs font-medium mb-2">Confirm Password:</label>
-                        <input
-                            type="password"
-                            id="confirm_password"
-                            className="w-full px-3 py-1 border border-gray-400 rounded"
-                            value={confirm_password}
-                            onChange={(event) => setConfirmPassword(event.target.value)}
-                            required
-                        />
-                    </div>
-
-                    <div className="flex flex-row justify-center text-sm">I agree with the&nbsp;
-                        <Link href="terms-and-conditions">
-                            <div className="text-[#7622FF] underline">Terms and Conditions</div>
-                        </Link>
-                    </div>
-
-                    <div className="flex flex-row justify-center text-sm text-red-500 mb-4">{error}</div>
-
-                    <div className="flex flex-row justify-center pt-5">
-                        <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-                            Create Account
-                        </button>
-                    </div>
-
-                </form>
-
-            </div>
-
-
-
+  return (
+    <div className="bg-[#C4D7F8] h-screen">
+      <div className="flex flex-row pt-5">
+        <div className="basis-1/5 z-50 text-center">
+          <a className="text-lg font-semibold text-white">Essential</a>
+          <a className="text-xl font-semibold text-[#022568]">ERP</a>
         </div>
-    )
+      </div>
+
+      <div className="flex flex-row justify-center pt-16">
+        <Logo01 />
+      </div>
+
+      <div className="flex flex-row justify-center text-center pt-5 text-sm">
+        <div className="flex flex-col">
+          <div>Create an account with email</div>
+          <div>
+            Already have an account?{" "}
+            <Link classname="text-[#3e399c] underline" href={"/login"}>
+              Login
+            </Link>{" "}
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-row justify-center">
+        <form onSubmit={handleSubmit} className="w-full max-w-sm p-6">
+          <div className="mb-4">
+            <label htmlFor="text" className="text-xs font-medium mb-2">
+              Company Name:
+            </label>
+            <input
+              type="text"
+              id="com_name"
+              className="w-full px-3 py-1 border border-gray-400 rounded"
+              value={com_name}
+              onChange={(event) => setCompanyName(event.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email" className="text-xs font-medium mb-2">
+              Company Email:
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="w-full px-3 py-1 border border-gray-400 rounded"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label htmlFor="password" className="text-xs font-medium mb-2">
+              Password:
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="w-full px-3 py-1 border border-gray-400 rounded"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label htmlFor="password" className="text-xs font-medium mb-2">
+              Confirm Password:
+            </label>
+            <input
+              type="password"
+              id="confirm_password"
+              className="w-full px-3 py-1 border border-gray-400 rounded"
+              value={confirm_password}
+              onChange={(event) => setConfirmPassword(event.target.value)}
+              required
+            />
+          </div>
+
+          <div className="flex flex-row justify-center text-sm">
+            <input
+              checked={isChecked}
+              onChange={handleCheckboxChange}
+              type="checkbox"
+              value=""
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            />
+            <label
+              for="checkbox-1"
+              className="ml-2 text-sm font-medium text-gray-900 dark:text-black dark:hover:text-300"
+            >
+              I agree with the{" "}
+              <Link
+                href="terms-and-conditions"
+                className="text-[#7622FF] underline"
+              >
+                Terms and Conditions
+              </Link>
+            </label>
+          </div>
+
+          <div className="flex flex-row justify-center text-sm text-red-500 mb-4">
+            {error}
+          </div>
+
+          <div className="flex flex-row justify-center pt-5">
+            <button
+              type="submit"
+              className="px-4 py-2 bg-indigo-500 text-white rounded hover:bg-blue-600"
+            >
+              Create Account
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 function submitForm(email) {
-    // Pretend it's hitting the network.
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            let shouldError = email.toLowerCase() !== 'kk.opoku@outlook.com'
-            if (shouldError) {
-                reject(new Error('Good guess but a wrong answer. Try again!'));
-                console.log("wrong mf")
-            } else {
-                resolve();
-            }
-        }, 1500);
-    });
+  // Pretend it's hitting the network.
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let shouldError = email.toLowerCase() !== "kk.opoku@outlook.com";
+      if (shouldError) {
+        reject(new Error("Good guess but a wrong answer. Try again!"));
+        console.log("wrong mf");
+      } else {
+        resolve();
+      }
+    }, 1500);
+  });
 }
