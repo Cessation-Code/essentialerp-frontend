b06456d0255eb20caaf909662867d78ac693b0f2
edem <unreshablekhon@gmail.com>
2023-06-11 15:34:40 +0000

    contact page update

--- pages/contact.js
+++ pages/contact.js
@@ -1,18 +1,11 @@
-import React, { useState } from "react";
 import Link from "next/link";
 import {
   EmailIcon,
   PhoneIcon,
-  DiagonalObject,
+  ContactUsIcon
 } from "../public/icons/contact/contacticons";
 
 const ContactUs = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setemail] = useState("");
-  const [message, setMessage] = useState("");
-
-
 
   const handleEmailCheck = () => {
     const mailtoUrl = `mailto:${'kk.opoku@outlook.com'}?subject=${encodeURIComponent('subject')}`;
@@ -20,7 +13,7 @@ const ContactUs = () => {
   };
   
   return (
-    <div className="page-container flex h-full  bg-[#DACDF0]">
+    <div className="page-container flex h-screen  bg-[#DACDF0]">
       
       <div className="sidebar-container pl-6">
       <Link href="/" className="text-lg font-semibold text-white">
@@ -32,16 +25,18 @@ const ContactUs = () => {
     </div>
 
       {/* Main Content */}
-      <div className="w-5/6 pt-28 text-heading flex-col justify-center">
-        {/* <div className="ml-12 z-20 absolute left-0 right-0 "> */}
+      <div className="w-5/6 pt-28 text-heading flex-col justify-center border-separate">
+      <div className="container mx-auto max-w-fit max-h-fit bg-slate-200 rounded-lg shadow-lg p-8">
         <div className=" flex flex-col  items-center pt-4">
           <p className=" text-3xl mb-10 text-[#B48DF3]">CONTACT US</p>
           <p className=" text-2xl mb-10 text-black">
             We'd Love to hear from you!
           </p>
 
+     <div className="mb-10"><ContactUsIcon/></div>   
+
           {/* Static contact information */}
-          <div className="icons-container flex flex-row pb-12 gap-20">
+          <div className="icons-container flex flex-row pb-12 gap-20 mb-0">
             <div className="email-item-container flex flex-col items-center">
               <EmailIcon className="mb-4" />
               <p className="text-center">Email</p>
@@ -58,63 +53,14 @@ const ContactUs = () => {
             </div>
           </div>
 
-          {/* Contact form */}
-          <form onSubmit={handleEmailCheck}>
-            <div className="flex gap-4">
-              <div className="flex flex-col">
-                <label htmlFor="firstName">First Name</label>
-                <input
-                  type="text"
-                  id="firstName"
-                  placeholder="e.g. John"
-                  value={firstName}
-                  onChange={(e) => setFirstName(e.target.value)}
-                  className="border border-gray-300 rounded-md py-2 px-3"
-                />
-              </div>
-              <div className="flex flex-col">
-                <label htmlFor="lastName">Last Name</label>
-                <input
-                  type="text"
-                  id="lastName"
-                  placeholder="e.g. Smith"
-                  value={lastName}
-                  onChange={(e) => setLastName(e.target.value)}
-                  className="border border-gray-300 rounded-md py-2 px-3"
-                />
-              </div>
-            </div>
-            <div className="flex flex-col mt-4">
-              <label htmlFor="email">Email</label>
-              <input
-                type="text"
-                id="email"
-                placeholder="e.g. johnsmith@gmail.com"
-                value={email}
-                onChange={(e) => setemail(e.target.value)}
-                className="border border-gray-300 rounded-md py-2 px-3"
-              />
-            </div>
-            <div className="flex flex-col mt-4">
-              <label htmlFor="message">Message</label>
-              <textarea
-                id="message"
-                rows="6"
-                value={message}
-                placeholder="e.g. Hello, I would like to..."
-                onChange={(e) => setMessage(e.target.value)}
-                className="border border-gray-300 rounded-md py-2 px-3"
-              ></textarea>
-            </div>
-            <button className="w-38 py-2 px-4 mt-8 rounded-xl shadow-lg text-black bg-[#B48DF3]  item-center font-semibold hover:bg-[#9747FF]" type="submit">
-              Send Message
+           
+            <button onClick={handleEmailCheck} className="w-38 py-2 px-4 mt-8 rounded-xl shadow-lg text-black bg-[#B48DF3]  item-center font-semibold hover:bg-[#9747FF]" type="submit">
+              Send Us an email
             </button>
-          </form>
-        </div>
+          </div>
       </div>
     </div>
-
-    // /div>
+    </div>
   );
 };
 
