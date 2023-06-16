937adfe2ba2a18af189ea12e2ce44feb6f67afac
kkopoku <kk.opoku@outlook.com>
2023-06-16 19:11:43 +0000

    shifted logo in navbar

--- components/layouts/authenticated_layout/navbar.js
+++ components/layouts/authenticated_layout/navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
 
   return (
     <div className="flex flex-row w-full bg-[#DACDF0] justify-between p-4">
-      <div className="sidebar-container mt-3 pl-32">
+      <div className="mt-3">
         <Link href="/" className="text-lg font-semibold text-[#B48DF3]">
           Essential
         </Link>
