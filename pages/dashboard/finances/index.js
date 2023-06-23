dfd9b5d0f4512175b5644d11b38cbde0f8c0c547
jakee0011 <unreshablekhon@gmail.com>
2023-06-23 04:39:25 +0000

    some content for each page

--- pages/dashboard/finances/index.js
+++ pages/dashboard/finances/index.js
@@ -1,10 +1,143 @@
-import React from "react";
+import React, { useState } from "react";
 import AuthenticatedLayout from "../../../components/layouts/authenticated_layout/authenticated_layout";
 
 const Finance = () => {
+  const [invoices, setInvoices] = useState([
+    { id: 1, title: "Invoice 1" },
+    { id: 2, title: "Invoice 2" },
+    { id: 3, title: "Invoice 3" },
+  ]);
+
+  const [expenses, setExpenses] = useState([
+    { id: 1, title: "Expense 1" },
+    { id: 2, title: "Expense 2" },
+    { id: 3, title: "Expense 3" },
+  ]);
+
+  const [calculations, setCalculations] = useState([
+    { id: 1, title: "Calculation 1" },
+    { id: 2, title: "Calculation 2" },
+    { id: 3, title: "Calculation 3" },
+  ]);
+
+  const performCalculations = () => {
+    // Perform finance calculations based on invoices and expenses data
+    const totalInvoices = invoices.length;
+    const totalExpenses = expenses.length;
+    const totalCalculations = calculations.length;
+
+    const results = [
+      { id: 1, title: `Total Invoices: ${totalInvoices}` },
+      { id: 2, title: `Total Expenses: ${totalExpenses}` },
+      { id: 3, title: `Total Calculations: ${totalCalculations}` },
+    ];
+
+    setCalculations(results);
+  };
+
+  const addNewInvoice = () => {
+    const newInvoiceId = invoices.length + 1;
+    const newInvoice = { id: newInvoiceId, title: `Invoice ${newInvoiceId}` };
+    setInvoices([...invoices, newInvoice]);
+  };
+
+  const addNewExpense = () => {
+    const newExpenseId = expenses.length + 1;
+    const newExpense = { id: newExpenseId, title: `Expense ${newExpenseId}` };
+    setExpenses([...expenses, newExpense]);
+  };
+
+  const deleteInvoice = (id) => {
+    const updatedInvoices = invoices.filter((invoice) => invoice.id !== id);
+    setInvoices(updatedInvoices);
+  };
+
+  const deleteExpense = (id) => {
+    const updatedExpenses = expenses.filter((expense) => expense.id !== id);
+    setExpenses(updatedExpenses);
+  };
+
   return (
     <div>
-      <AuthenticatedLayout>Finance Page Component</AuthenticatedLayout>
+      <AuthenticatedLayout>
+        <div className="flex flex-wrap">
+          <div className="w-full md:w-3/4">
+            <div className="bg-white rounded-lg p-4 mb-4 h-{50%} overflow-y-auto">
+              <h3 className="text-lg font-bold mb-2">Invoices</h3>
+              <table className="w-full border border-gray-300">
+                <thead>
+                  <tr className="bg-gray-100">
+                    <th className="border-b border-gray-300 px-4 py-2">ID</th>
+                    <th className="border-b border-gray-300 px-4 py-2">Title</th>
+                    <th className="border-b border-gray-300 px-4 py-2">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {invoices.map((invoice) => (
+                    <tr key={invoice.id}>
+                      <td className="border-b border-gray-300 px-4 py-2">{invoice.id}</td>
+                      <td className="border-b border-gray-300 px-4 py-2">{invoice.title}</td>
+                      <td className="border-b border-gray-300 px-4 py-2">
+                        <button className="btn btn-primary mr-2">Edit</button>
+                        <button className="btn btn-danger" onClick={() => deleteInvoice(invoice.id)}>
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <button className="btn btn-primary mt-4 ml-auto" onClick={addNewInvoice}>
+                Add Invoice
+              </button>
+            </div>
+            <div className="bg-white rounded-lg p-4 h-{50%} overflow-y-auto">
+              <h3 className="text-lg font-bold mb-2">Expenses</h3>
+              <table className="w-full border border-gray-300">
+                <thead>
+                  <tr className="bg-gray-100">
+                    <th className="border-b border-gray-300 px-4 py-2">ID</th>
+                    <th className="border-b border-gray-300 px-4 py-2">Title</th>
+                    <th className="border-b border-gray-300 px-4 py-2">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {expenses.map((expense) => (
+                    <tr key={expense.id}>
+                      <td className="border-b border-gray-300 px-4 py-2">{expense.id}</td>
+                      <td className="border-b border-gray-300 px-4 py-2">{expense.title}</td>
+                      <td className="border-b border-gray-300 px-4 py-2">
+                        <button className="btn btn-primary mr-2">Edit</button>
+                        <button className="btn btn-danger" onClick={() => deleteExpense(expense.id)}>
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <button className="btn btn-primary mt-4 ml-auto" onClick={addNewExpense}>
+                Add Expense
+              </button>
+            </div>
+          </div>
+          <div className="w-full md:w-1/4 my-4 px-3">
+            <div className="bg-red-400 rounded-lg p-4 h-full overflow-y-auto">
+              <h3 className="text-lg font-bold mb-2">Calculations</h3>
+              <ul>
+                {calculations.map((calculation) => (
+                  <li key={calculation.id}>{calculation.title}</li>
+                ))}
+              </ul>
+              <div className="text-center mt-4">
+                <button className="btn btn-primary" onClick={performCalculations}>
+                  Perform Calculations
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </AuthenticatedLayout>
     </div>
   );
 };
--- pages/dashboard/inventory/index.js
+++ pages/dashboard/inventory/index.js
@@ -1,15 +1,297 @@
-import React from 'react'
-import AuthenticatedLayout from '../../../components/layouts/authenticated_layout/authenticated_layout'
+import React, { useState } from "react";
+import { FaTrash, FaCheck, FaSearch, FaEdit } from "react-icons/fa";
+import AuthenticatedLayout from "../../../components/layouts/authenticated_layout/authenticated_layout";
 
 const Inventory = () => {
+  const [inventoryData, setInventoryData] = useState([
+    {
+      id: 1,
+      productName: "Waakye",
+      category: "Breakfast",
+      units: 50,
+      cost: 50,
+      
+    },
+    {
+      id: 2,
+      productName: "Bacon",
+      category: "Lunch",
+      units: 100,
+      cost: 100,
+          },
+    {
+      id: 3,
+      productName: "Tombrown",
+      category: "Brunch",
+      units: 75,
+      cost: 75,
+      
+    },
+    {
+      id: 4,
+      productName: "Gobe",
+      category: "Dessert",
+      units: 120,
+      cost: 120,
+      
+    },
+  ]);
+  const [newItem, setNewItem] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [editingItemId, setEditingItemId] = useState(null);
+
+  const handleEdit = (id, field, value) => {
+    const updatedData = inventoryData.map((item) => {
+      if (item.id === id) {
+        return { ...item, [field]: value };
+      }
+      return item;
+    });
+    setInventoryData(updatedData);
+  };
+
+  const handleAddItem = () => {
+    const newId =
+      inventoryData.length > 0
+        ? inventoryData[inventoryData.length - 1].id + 1
+        : 1;
+    setNewItem({
+      id: newId,
+      productName: "",
+      category: "Category 1",
+      units: "",
+      cost: "",
+    });
+  };
+
+  const handleApproveItem = () => {
+    setInventoryData((prevData) => [...prevData, newItem]);
+    setNewItem(null);
+  };
+
+  const handleRemoveItem = (id) => {
+    const updatedData = inventoryData.filter((item) => item.id !== id);
+    setInventoryData(updatedData);
+  };
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleEditItem = (id) => {
+    setEditingItemId(id);
+  };
+
+  const handleApproveEdit = (id) => {
+    setEditingItemId(null);
+  };
+
+  const filteredData = inventoryData.filter(
+    (item) =>
+      item.productName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      item.supplier.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div>
-            <AuthenticatedLayout>
-
-      Inventory Page Component
-    </AuthenticatedLayout>
+      <AuthenticatedLayout>
+        <div className="overflow-x-auto overflow-y-auto p-4 mt-16 mx-10 border-4 rounded-3xl">
+          <div className="flex justify-between items-center mb-4">
+            <h2 className="text-2xl font-bold">Inventory</h2>
+            <div className="flex items-center space-x-4">
+              <div className="relative">
+                <input
+                  type="text"
+                  className="input input-sm pr-10"
+                  placeholder="Search..."
+                  value={searchTerm}
+                  onChange={handleSearch}
+                />
+                <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+                  <FaSearch className="text-gray-400" />
+                </div>
+              </div>
+              {newItem ? (
+                <button className="btn btn-icon" onClick={handleApproveItem}>
+                  <FaCheck />
+                </button>
+              ) : (
+                <button className="btn btn-primary" onClick={handleAddItem}>
+                  Add Item
+                </button>
+              )}
+            </div>
+          </div>
+          <table className="table table-xs bg-white">
+            <thead>
+              <tr>
+                <th className="bg-gray-200 rounded-tl-md">ID</th>
+                <th className="bg-gray-200">Product Name</th>
+                <th className="bg-gray-200">Category</th>
+                <th className="bg-gray-200">Units</th>
+                <th className="bg-gray-200">Cost</th>
+                <th className="bg-gray-200 rounded-tr-md">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredData.map((item) => (
+                <tr key={item.id}>
+                  <th>{item.id}</th>
+                  <td>
+                    {editingItemId === item.id ? (
+                      <input
+                        type="text"
+                        className="input input-sm"
+                        value={item.productName}
+                        onChange={(e) =>
+                          handleEdit(item.id, "productName", e.target.value)
+                        }
+                      />
+                    ) : (
+                      item.productName
+                    )}
+                  </td>
+                  <td>
+                    {editingItemId === item.id ? (
+                      <select
+                        className="input input-sm"
+                        value={item.category}
+                        onChange={(e) =>
+                          handleEdit(item.id, "category", e.target.value)
+                        }
+                      >
+                        <option value="Category 1">Category 1</option>
+                        <option value="Category 2">Category 2</option>
+                        <option value="Category 3">Category 3</option>
+                        <option value="Category 4">Category 4</option>
+                      </select>
+                    ) : (
+                      item.category
+                    )}
+                  </td>
+                  <td>
+                    {editingItemId === item.id ? (
+                      <input
+                        type="number"
+                        className="input input-sm"
+                        value={item.units}
+                        onChange={(e) =>
+                          handleEdit(item.id, "units", e.target.value)
+                        }
+                      />
+                    ) : (
+                      item.units
+                    )}
+                  </td>
+                  <td>
+                    {editingItemId === item.id ? (
+                      <input
+                        type="number"
+                        className="input input-sm"
+                        value={item.cost}
+                        onChange={(e) =>
+                          handleEdit(item.id, "cost", e.target.value)
+                        }
+                      />
+                    ) : (
+                      item.cost
+                    )}
+                  </td>
+                  <td>
+                    {editingItemId === item.id ? (
+                      <button
+                        className="btn btn-icon"
+                        onClick={() => handleApproveEdit(item.id)}
+                      >
+                        <FaCheck />
+                      </button>
+                    ) : (
+                      <>
+                        <button
+                          className="btn btn-icon"
+                          onClick={() => handleEditItem(item.id)}
+                        >
+                          <FaEdit />
+                        </button>
+                        <span className="mx-1"></span>
+                        {/* Add some margin between buttons */}
+                        <button
+                          className="btn btn-icon"
+                          onClick={() => handleRemoveItem(item.id)}
+                        >
+                          <FaTrash />
+                        </button>
+                      </>
+                    )}
+                  </td>
+                </tr>
+              ))}
+              {newItem && (
+                <tr>
+                  <th>{newItem.id}</th>
+                  <td>
+                    <input
+                      type="text"
+                      className="input input-sm"
+                      placeholder="Product Name"
+                      value={newItem.productName}
+                      onChange={(e) =>
+                        setNewItem({ ...newItem, productName: e.target.value })
+                      }
+                    />
+                  </td>
+                  <td>
+                    <select
+                      className="input input-sm"
+                      value={newItem.category}
+                      onChange={(e) =>
+                        setNewItem({ ...newItem, category: e.target.value })
+                      }
+                    >
+                      <option value="Category 1">Category 1</option>
+                      <option value="Category 2">Category 2</option>
+                      <option value="Category 3">Category 3</option>
+                      <option value="Category 4">Category 4</option>
+                    </select>
+                  </td>
+                  <td>
+                    <input
+                      type="number"
+                      className="input input-sm"
+                      placeholder="Units"
+                      value={newItem.units}
+                      onChange={(e) =>
+                        setNewItem({ ...newItem, units: e.target.value })
+                      }
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="number"
+                      className="input input-sm"
+                      placeholder="Cost"
+                      value={newItem.cost}
+                      onChange={(e) =>
+                        setNewItem({ ...newItem, cost: e.target.value })
+                      }
+                    />
+                  </td>
+                  <td>
+                    <button
+                      className="btn btn-icon"
+                      onClick={handleApproveItem}
+                    >
+                      <FaCheck />
+                    </button>
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+      </AuthenticatedLayout>
     </div>
-  )
-}
+  );
+};
 
-export default Inventory
+export default Inventory;
