dedaf3e7818edd488926e833053f2b1883fdaae6
kkopoku <kk.opoku@outlook.com>
2023-07-14 12:25:51 +0000

    fixed withAuth usage in finance page

--- pages/dashboard/finances/index.js
+++ pages/dashboard/finances/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import AuthenticatedLayout from "../../../components/layouts/authenticated_layout/authenticated_layout";
 import { faTrash, faEdit, faCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import withAuth from "../../../components/withAuth";
 
 const Finance = () => {
   const [incomeEntries, setIncomeEntries] = useState([
@@ -114,7 +114,6 @@ const Finance = () => {
 
   return (
     <div>
-      <AuthenticatedLayout>
         <div className="flex flex-wrap">
           <div className="w-full">
             <div className="bg-white rounded-lg p-4 mb-4">
@@ -354,9 +353,8 @@ const Finance = () => {
             </div>
           </div>
         </div>
-      </AuthenticatedLayout>
     </div>
   );
 };
 
-export default Finance;
+export default withAuth(Finance);
