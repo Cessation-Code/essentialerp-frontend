8ba1237eb1666941bebd798d4e5a46dd1313e090
kkopoku <kk.opoku@outlook.com>
2023-07-19 18:30:25 +0000

    fetching expense list

--- pages/dashboard/finances/expenses.js
+++ pages/dashboard/finances/expenses.js
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import SearchButton from "../../../components/search";
 import Modal from "./addExpenseModal";
+import LoadingSpinner from "../../../components/loadingSpinner";
 
 const ExpenseTable = () => {
 
+  // dummy data
   const expenseEntries = [{
     id: 1,
     date: "2023-05-02",
@@ -76,9 +78,33 @@ const ExpenseTable = () => {
     amount: 1500,
     editMode: false,
   },
-]
+  ]
 
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [expenseItems, setExpenseItems] = useState("")
+
+  // get expense items
+  useEffect(() => {
+    async function getExpenseItems() {
+      try {
+        // get expense items
+        await fetch("http://localhost:8000/api/v1/expense/", {
+          method: "GET",
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem('token')}`
+          },
+        }
+        ).then(response => response.json()).then(data => {
+          setExpenseItems(data.expenses)
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getExpenseItems()
+  }, [])
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -88,62 +114,70 @@ const ExpenseTable = () => {
     setIsModalOpen(false);
   };
 
-  return (
-    <div className="w-full px-6">
+  console.log(expenseItems)
 
-      <div className="flex flex-row justify-between mb-4">
-        <h3 className="text-3xl text ml-2 font-semibold">Expense Table</h3>
-        <div className="flex flex-row items-baseline">
-          <SearchButton />
-          <button className="btn" onClick={openModal}>
-            Add Expense
-          </button>
+  // show loading spinner if expense items are not loaded
+  if(!expenseItems){
+    return <LoadingSpinner />
+  }else{
+    return (
+      <div className="w-full px-6">
+  
+        <div className="flex flex-row justify-between mb-4">
+          <h3 className="text-3xl text ml-2 font-semibold">Expense Table</h3>
+          <div className="flex flex-row items-baseline">
+            <SearchButton />
+            <button className="btn" onClick={openModal}>
+              Add Expense
+            </button>
+          </div>
         </div>
-      </div>
-
-      <Modal isOpen={isModalOpen} onClose={closeModal} />
-
-      <div className="max-h-[55vh] overflow-y-auto custom-scrollbar">
-        <table className="w-[98%] border border-gray-300 ">
-          <thead>
-            <tr className="bg-gray-100">
-              <th className="border-b border-gray-300 px-4 py-2 text-left">
-                Date
-              </th>
-              <th className="border-b border-gray-300 px-4 py-2 text-left">
-                Category
-              </th>
-              <th className="border-b border-gray-300 px-4 py-2 text-left">
-                Amount
-              </th>
-              <th className="border-b border-gray-300 px-4 py-2 text-left"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {expenseEntries.map((entry) => (
-              <tr key={entry.id}>
-                <td className="border-b border-gray-300 px-4 py-2">
-                  {entry.date}
-                </td>
-                <td className="border-b border-gray-300 px-4 py-2">
-                  {entry.category}
-                </td>
-                <td className="border-b border-gray-300 px-4 py-2">
-                  {entry.amount}
-                </td>
-                <td className="border-b border-gray-300 py-2">
-                  <button className="btn-icon" onClick={() => console.log('trash button clicked')}>
-                    <FontAwesomeIcon icon={faCircleInfo} />
-                  </button>
-                </td>
+  
+        <Modal isOpen={isModalOpen} onClose={closeModal} />
+  
+        <div className="max-h-[55vh] overflow-y-auto custom-scrollbar">
+          <table className="w-[98%] border border-gray-300 ">
+            <thead>
+              <tr className="bg-gray-100">
+                <th className="border-b border-gray-300 px-4 py-2 text-left">
+                  Date
+                </th>
+                <th className="border-b border-gray-300 px-4 py-2 text-left">
+                  Category
+                </th>
+                <th className="border-b border-gray-300 px-4 py-2 text-left">
+                  Amount
+                </th>
+                <th className="border-b border-gray-300 px-4 py-2 text-left"></th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {expenseEntries.map((entry) => (
+                <tr key={entry.id}>
+                  <td className="border-b border-gray-300 px-4 py-2">
+                    {entry.date}
+                  </td>
+                  <td className="border-b border-gray-300 px-4 py-2">
+                    {entry.category}
+                  </td>
+                  <td className="border-b border-gray-300 px-4 py-2">
+                    {entry.amount}
+                  </td>
+                  <td className="border-b border-gray-300 py-2">
+                    <button className="btn-icon" onClick={() => console.log('trash button clicked')}>
+                      <FontAwesomeIcon icon={faCircleInfo} />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+  
       </div>
-
-    </div>
-  );
+    );
+  }
+  
 };
 
 export default ExpenseTable;
