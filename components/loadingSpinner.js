702ae2fd10971f3d45f7bb09664dfaa6972beea8
kkopoku <kk.opoku@outlook.com>
2023-07-22 05:00:58 +0000

    added grey bg to loading spinner

--- components/loadingSpinner.js
+++ components/loadingSpinner.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const LoadingSpinner = () => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center">
-      <div className="animate-spin rounded-full h-20 w-20 border-b-4 border-gray-900"></div>
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-700 bg-opacity-60">
+      <div className="animate-spin rounded-full h-20 w-20 border-b-4 border-gray-900 "></div>
     </div>
   );
 };
