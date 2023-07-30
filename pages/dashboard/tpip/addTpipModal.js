6c4400a7d72a606d13b514d11e111bb5a9b2c561
kkopoku <kk.opoku@outlook.com>
2023-07-30 01:41:23 +0000

    finisheg tpip ui

---
+++ pages/dashboard/tpip/addTpipModal.js
@@ -0,0 +1,161 @@
+import React, { useState, useEffect } from 'react';
+import ModalLayout from '../../../components/layouts/modal_layout';
+import { useRouter } from 'next/router';
+
+function AddTPIPModal({ isOpen, onClose }) {
+
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [secret_key, setSecret_key] = useState("");
+
+  const router = useRouter();
+
+  const handleSubmit = async (event) => {
+
+//     const expenseData = {
+//       name: name,
+//       amount: amount,
+//       description: description
+//     }
+
+//     event.preventDefault();
+
+//     if (!name || !amount || !description) {
+//       setError("Please fill all fields!");
+//     }
+//     else {
+//       setIsLoading(true);
+//       // create expense
+//       try {
+//         const response = await fetch("https://essential-erp-10cac5b0da28.herokuapp.com/api/v1/expense/createExpense", {
+//           method: "POST",
+//           headers: {
+//             "Access-Control-Allow-Origin": "*",
+//             "Content-Type": "application/json",
+//             Authorization: `Bearer ${localStorage.getItem('token')}`
+//           },
+//           body: JSON.stringify(expenseData),
+//         });
+//         if (response.ok) {
+//           console.log("Expense created successfully!");
+//           setName("");
+//           setAmount("");
+//           setDescription("");
+//           onClose();
+//           setIsLoading(false);
+//           setError("");
+//           window.location.hash = '#expenses'
+//           router.reload();
+//         } else {
+//           setIsLoading(false);
+//           setError("An Error Occured whiles creating expense!");
+//         }
+//       } catch (error) {
+//         setIsLoading(false);
+//         console.log(error);
+//         setError(error);
+//       }
+//     }
+  }
+
+  const closeModal = async (event) => {
+    event.preventDefault();
+    setName("");
+    setEmail("");
+    setError("");
+    setIsLoading(false)
+    onClose();
+  }
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <ModalLayout header="TPIP" closeModal={closeModal}>
+      {/* Form */}
+      <form onSubmit={handleSubmit}>
+        <div className="flex flex-row gap-2">
+
+          <div className="flex flex-col basis-1/2 mb-4">
+            <label className="text-xs mb-1 text-gray-400">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              placeholder="name"
+              className="w-full h-6 bg-white rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-sm outline-none text-gray-700 px-3 transition-colors duration-200 ease-in-out"
+              required
+              value={name}
+              onChange={(event) => {
+                setName(event.target.value)
+                setError("")
+              }}
+            />
+          </div>
+
+          <div className="flex flex-col basis-1/2 mb-4">
+            <label className="text-xs mb-1 text-gray-400">
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              placeholder="email"
+              className="w-full h-6 bg-white rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-sm outline-none text-gray-700 pl-3 transition-colors duration-200 ease-in-out"
+              required // Make the field required
+              value={email}
+              onChange={(event) => {
+                setEmail(event.target.value)
+                setError("")
+              }}
+            />
+          </div>
+
+        </div>
+        <div className='flex flex-row'>
+          <div className="flex basis-full flex-col">
+            <label className="text-xs mb-1 text-gray-400">Password</label>
+            <input 
+              type="text"
+              id="password"
+              placeholder="password"
+              className="w-full h-6 bg-white rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-sm outline-none text-gray-700 pl-3 transition-colors duration-200 ease-in-out"
+              required // Make the field required
+              value={password}
+              onChange={(event) => {
+                setPassword(event.target.value)
+                setError("")
+              }}
+            />
+          </div>
+        </div>
+      </form>
+
+      <div className="flex flex-row">
+        <div className="flex basis-3/5 justify-start text-xs text-red-500 mt-5">
+          {error && <p>{error}</p>}
+        </div>
+        <div className="flex basis-2/5 justify-end">
+          {isLoading ? (null) :
+            (<div className='flex flex-row'>
+              <button type="reset" className="bg-transparent text-black text-sm py-1 px-4 mt-4" onClick={closeModal}>
+                Cancel
+              </button>
+              <button type="submit" className="bg-[#C3A2FA] hover:bg-blue-600 text-white text-sm py-1 px-4 rounded mt-4" onClick={handleSubmit}>
+                Confirm
+              </button>
+            </div>)}
+        </div>
+      </div>
+
+    </ModalLayout>
+  );
+}
+
+export default AddTPIPModal;
--- pages/dashboard/tpip/tpip.js
+++ pages/dashboard/tpip/tpip.js
@@ -2,142 +2,142 @@ import React from "react";
 import { useState } from "react";
 import SearchButton from "../../../components/search";
 import ViewTPIPModal from "./viewTpipModal";
+import AddTPIPModal from "./addTpipModal";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
 
-export const Tpip = {
-  tpipData: [
-    {
-      id: 1,
-      name: "TPIP 1",
-      date: "2023-05-01",
-      category: "TPIPs",
-      amount: 1000,
-      editMode: false,
-    },
-    {
-      id: 2,
-      name: "TPIP 2",
-      date: "2023-05-03",
-      category: "TPIPs",
-      amount: 2000,
-      editMode: false,
-    },
-    {
-      id: 3,
-      name: "TPIP 3",
-      date: "2023-05-05",
-      category: "TPIPs",
-      amount: 1500,
-      editMode: false,
-    },
-    {
-      id: 4,
-      name: "TPIP 4",
-      date: "2023-05-07",
-      category: "TPIPs",
-      amount: 1800,
-      editMode: false,
-    },
-    {
-      id: 5,
-      name: "TPIP 5",
-      date: "2023-05-09",
-      category: "TPIPs",
-      amount: 1200,
-      editMode: false,
-    },
-    {
-      id: 6,
-      name: "TPIP 6",
-      date: "2023-05-11",
-      category: "TPIPs",
-      amount: 2500,
-      editMode: false,
-    },
-  ],
-};
+const tpipData = [
+  {
+    _id: 1,
+    name: "John Doe",
+    email: "john.doe@example.com",
+    password: "pass123",
+    secret_key: "abc123xyz"
+  },
+  {
+    _id: 2,
+    name: "Jane Smith",
+    email: "jane.smith@example.com",
+    password: "hello@321",
+    secret_key: "def456uvw"
+  },
+  {
+    _id: 3,
+    name: "Michael Johnson",
+    email: "michael.johnson@example.com",
+    password: "qwerty987",
+    secret_key: "ghi789rst"
+  },
+  {
+    _id: 4,
+    name: "Emily Brown",
+    email: "emily.brown@example.com",
+    password: "myp@ssword",
+    secret_key: "jkl012pqr"
+  },
+  {
+    _id: 5,
+    name: "Robert Lee",
+    email: "robert.lee@example.com",
+    password: "secretpass",
+    secret_key: "mno345stu"
+  },
+  {
+    _id: 6,
+    name: "Sophia Wang",
+    email: "sophia.wang@example.com",
+    password: "p@ssw0rd!",
+    secret_key: "vwx678yz0"
+  },
+]
+
 const TPIP = () => {
   const [selectedRowData, setSelectedRowData] = useState("");
   const [viewTPIPModal, setViewTPIPModal] = useState(false);
+  const [addTPIPModal, setAddTPIPModal] = useState(false);
 
   const openViewTPIPModal = () => {
     setViewTPIPModal(true);
   };
 
   const closeViewTPIPModal = () => {
-        setViewTPIPModal(false);
+    setViewTPIPModal(false);
   };
 
+  const openAddTPIPModal = () => {
+    setAddTPIPModal(true);
+  }
+
+  const closeAddTPIPModal = () => {
+    setAddTPIPModal(false);
+  }
+
   return (
-    
-      <div className="w-full px-6">
-        <div className="flex flex-row justify-between mb-4">
-          <h3 className="text-3xl">TPIP Table</h3>
-          <div className="flex flex-row items-baseline">
-            <SearchButton />
-            <button
-              className="btn"
-              onClick={null}
-            >
-              Add TPIP
-            </button>
-          </div>
-        </div>
 
-        {viewTPIPModal && (
-          <ViewTPIPModal
-            onClose={closeViewTPIPModal}
-            selectedRowData={selectedRowData}
-          />
-        )}
-
-        <div className="max-h-[55vh] overflow-y-auto custom-scrollbar">
-          <table className="w-[98%] border border-gray-300 mr-4">
-            <thead>
-              <tr className="bg-gray-100">
-                <th className="border-b border-gray-300 px-4 py-2 text-left">
-                  Name
-                </th>
-                <th className="border-b border-gray-300 px-4 py-2 text-left">
-                  Amount
-                </th>
-                <th className="border-b border-gray-300 px-4 py-2 text-left">
-                  Date
-                </th>
-                <th className="border-b border-gray-300 px-4 py-2 text-left"></th>
-              </tr>
-            </thead>
-            <tbody>
-              {Tpip.tpipData.map((entry) => (
-                <tr key={entry.id}>
-                  <td className="border-b border-gray-300 px-4 py-2">
-                    {entry.name}
-                  </td>
-                  <td className="border-b border-gray-300 px-4 py-2">
-                    {entry.amount}
-                  </td>
-                  <td className="border-b border-gray-300 px-4 py-2">
-                    {entry.date}
-                  </td>
-                  <td className="border-b border-gray-300 py-2">
-                    <button
-                      className="btn-icon mr-2"
-                      onClick={() => {
-                        setSelectedRowData(entry);
-                        openViewTPIPModal();
-                      }}
-                    >
-                      <FontAwesomeIcon icon={faCircleInfo} />
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+    <div className="w-full px-6">
+      <div className="flex flex-row justify-between mb-4">
+        <h3 className="text-3xl">TPIP Table</h3>
+        <div className="flex flex-row items-baseline">
+          <SearchButton />
+          <button
+            className="btn"
+            onClick={() => {
+              setAddTPIPModal(true)
+            }}
+          >
+            Add TPIP
+          </button>
         </div>
       </div>
-    
+
+      {viewTPIPModal && (<ViewTPIPModal isOpen={setViewTPIPModal} onClose={closeViewTPIPModal} selectedRowData={selectedRowData} />)}
+      {addTPIPModal && <AddTPIPModal isOpen={openAddTPIPModal} onClose={closeAddTPIPModal} />}
+
+      <div className="max-h-[55vh] overflow-y-auto custom-scrollbar">
+        <table className="w-[98%] border border-gray-300 mr-4">
+          <thead>
+            <tr className="bg-gray-100">
+              <th className="border-b border-gray-300 px-4 py-2 text-left">
+                Name
+              </th>
+              <th className="border-b border-gray-300 px-4 py-2 text-left">
+                Email
+              </th>
+              <th className="border-b border-gray-300 px-4 py-2 text-left">
+                Secret Key
+              </th>
+              <th className="border-b border-gray-300 px-4 py-2 text-left"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {tpipData.map((entry) => (
+              <tr key={entry.name}>
+                <td className="border-b border-gray-300 px-4 py-2">
+                  {entry.name}
+                </td>
+                <td className="border-b border-gray-300 px-4 py-2">
+                  {entry.email}
+                </td>
+                <td className="border-b border-gray-300 px-4 py-2">
+                  {entry.secret_key}
+                </td>
+                <td className="border-b border-gray-300 py-2">
+                  <button
+                    className="btn-icon mr-2"
+                    onClick={() => {
+                      setSelectedRowData(entry);
+                      openViewTPIPModal();
+                    }}
+                  >
+                    <FontAwesomeIcon icon={faCircleInfo} />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 };
 
