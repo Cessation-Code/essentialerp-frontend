9eb7f0d1522420d8ebd4b9456584297597de600d
jake0011 <unreshablekhon@gmail.com>
2023-08-02 21:06:50 +0000

    some changes

--- pages/dashboard/inventory/report.js
+++ pages/dashboard/inventory/report.js
@@ -11,47 +11,10 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-const data = [
-  {
-    name: "Product 1",
-    stock: 2303,
-    price: 232,
-  },
-  {
-    name: "Product 2",
-    stock: 3000,
-    price: 2210,
-  },
-  {
-    name: "Product 3",
-    stock: 4000,
-    price: 2100,
-  },
-  {
-    name: "Product 4",
-    stock: 1203,
-    price: 212,
-  },
-  {
-    name: "Product 5",
-    stock: 2303,
-    price: 232,
-  },
-  {
-    name: "Product 6",
-    stock: 3000,
-    price: 2210,
-  },
-  {
-    name: "Product 7",
-    stock: 4000,
-    price: 2100,
-  },
-];
 
 const Report = ({ inventoryItems }) => {
   return (
-    <div className="bg-slate-100 w-[90%] m-4 rounded-[20px] shadow-lg overflow-hidden h-[80%] p-4">
+    <div className="bg-slate-100 w-[90%] m- rounded-[20px] shadow-lg overflow-hidden h-[80%] p-4">
       <h2 className="text-2xl font-bold text-center text-gray-800 pb-4">
         Inventory Report
       </h2>
