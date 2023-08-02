c533e3536e8a7f73208cad94bff5b7d519beb529
jake0011 <unreshablekhon@gmail.com>
2023-08-02 03:33:05 +0000

    search functionality

--- pages/dashboard/finances/expenses.js
+++ pages/dashboard/finances/expenses.js
@@ -7,37 +7,55 @@ import ViewExpenseModal from "./viewExpenseModal";
 import LoadingSpinner from "../../../components/loadingSpinner";
 
 const ExpenseTable = () => {
-
   const [isAddExpenseModalOpen, setIsAddExpenseModalOpen] = useState(false);
   const [isViewExpenseModalOpen, setIsViewExpenseModalOpen] = useState(false);
   const [selectedRowData, setSelectedRowData] = useState("");
   const [expenseItems, setExpenseItems] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchQuery, setSearchQuery] = useState(""); // Track the search query
 
   // get expense items
   useEffect(() => {
     async function getExpenseItems() {
       try {
         // get expense items
-        await fetch("https://essential-erp-10cac5b0da28.herokuapp.com/api/v1/expense/", {
-          method: "GET",
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${localStorage.getItem('token')}`
-          },
-        }
-        ).then(response => response.json()).then(data => {
-          // save it to state
-          setExpenseItems(data.expenses)
-          console.log(data.expenses)
-        });
+        const response = await fetch(
+          "https://essential-erp-10cac5b0da28.herokuapp.com/api/v1/expense/",
+          {
+            method: "GET",
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
+        const data = await response.json();
+        // save it to state
+        setExpenseItems(data.expenses);
+        console.log(data.expenses);
       } catch (error) {
         console.log(error);
       }
     }
-    getExpenseItems()
-  }, [])
+    getExpenseItems();
+  }, []);
 
+  const handleSearch = (event) => {
+    const query = event.target.value;
+    setSearchQuery(query); // Update the search query state
+    if (query.trim() === "") {
+      // If the search query is empty, reset the search results
+      setSearchResults([]);
+    } else {
+      // Include all elements which include the search query
+      const filteredData = expenseItems.filter((item) =>
+        item.name.toLowerCase().includes(query.toLowerCase())
+      );
+      setSearchResults(filteredData);
+    }
+  };
+  
 
   const openAddExpenseModal = () => {
     setIsAddExpenseModalOpen(true);
@@ -49,84 +67,101 @@ const ExpenseTable = () => {
 
   const openViewExpenseModal = () => {
     setIsViewExpenseModalOpen(true);
-  }
+  };
 
   const closeViewExpenseModal = () => {
     setIsViewExpenseModalOpen(false);
-  }
+  };
 
   function formatDate(expenseDate) {
     const date = new Date(expenseDate);
     return date.toLocaleDateString("en-US");
   }
 
-  // show loading spinner if expense aren't loaded yet
+  // show loading spinner if expense items aren't loaded yet
   if (!expenseItems) {
-    return <LoadingSpinner />
-  } else {
-    return (
-      <div className="w-full px-6">
-
-        <div className="flex flex-row justify-between mb-4">
-          <h3 className="text-3xl text ml-2 font-semibold">Expense Table</h3>
-          <div className="flex flex-row items-baseline">
-            <SearchButton />
-            <button className="btn" onClick={openAddExpenseModal}>
-              Add Expense
-            </button>
-          </div>
-        </div>
-
-        {isAddExpenseModalOpen && <AddExpenseModal isOpen={openAddExpenseModal} onClose={closeAddExpenseModal} />}
-        {isViewExpenseModalOpen && <ViewExpenseModal isOpen={openViewExpenseModal} onClose={closeViewExpenseModal} selectedRowData={selectedRowData} />}
-
-
-        <div className="max-h-[55vh] overflow-y-auto custom-scrollbar">
-          <table className="w-[98%] border border-gray-300 ">
-            <thead>
-              <tr className="bg-gray-100">
-                <th className="border-b border-gray-300 px-4 py-2 text-left">
-                  Name
-                </th>
-                <th className="border-b border-gray-300 px-4 py-2 text-left">
-                  Amount
-                </th>
-                <th className="border-b border-gray-300 px-4 py-2 text-left">
-                  Date
-                </th>
-                <th className="border-b border-gray-300 px-4 py-2 text-left"></th>
-              </tr>
-            </thead>
-            <tbody>
-              {expenseItems.map((entry) => (
-                <tr key={entry._id}>
-                  <td className="border-b border-gray-300 px-4 py-2">
-                    {entry.name}
-                  </td>
-                  <td className="border-b border-gray-300 px-4 py-2">
-                    {entry.amount}
-                  </td>
-                  <td className="border-b border-gray-300 px-4 py-2">
-                    {formatDate(entry.created_at)}
-                  </td>
-                  <td className="border-b border-gray-300 py-2">
-                    <button className="btn-icon" onClick={() => {
-                      setSelectedRowData(entry);
-                      openViewExpenseModal();
-                    }}>
-                      <FontAwesomeIcon icon={faCircleInfo} />
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-
-      </div>
-    );
+    return <LoadingSpinner />;
   }
 
+  return (
+    <div className="w-full px-6">
+      <div className="flex flex-row justify-between mb-4">
+        <h3 className="text-3xl text ml-2 font-semibold">Expense Table</h3>
+        <div className="flex flex-row items-baseline">
+          <SearchButton onSearch={handleSearch} />
+          <button className="btn" onClick={openAddExpenseModal}>
+            Add Expense
+          </button>
+        </div>
+      </div>
+
+      {isAddExpenseModalOpen && (
+        <AddExpenseModal isOpen={openAddExpenseModal} onClose={closeAddExpenseModal} />
+      )}
+      {isViewExpenseModalOpen && (
+        <ViewExpenseModal
+          isOpen={openViewExpenseModal}
+          onClose={closeViewExpenseModal}
+          selectedRowData={selectedRowData}
+        />
+      )}
+
+      <div className="max-h-[55vh] overflow-y-auto custom-scrollbar">
+        <table className="w-[98%] border border-gray-300 ">
+          <thead>
+            <tr className="bg-gray-100">
+              <th className="border-b border-gray-300 px-4 py-2 text-left">
+                Name
+              </th>
+              <th className="border-b border-gray-300 px-4 py-2 text-left">
+                Amount
+              </th>
+              <th className="border-b border-gray-300 px-4 py-2 text-left">
+                Date
+              </th>
+              <th className="border-b border-gray-300 px-4 py-2 text-left"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {(searchQuery.trim() === "" || searchResults.length === 0
+              ? expenseItems
+              : searchResults
+            ).map((entry) => (
+              <tr key={entry._id}>
+                <td className="border-b border-gray-300 px-4 py-2">
+                  {entry.name}
+                </td>
+                <td className="border-b border-gray-300 px-4 py-2">
+                  {entry.amount}
+                </td>
+                <td className="border-b border-gray-300 px-4 py-2">
+                  {formatDate(entry.created_at)}
+                </td>
+                <td className="border-b border-gray-300 py-2">
+                  <button
+                    className="btn-icon"
+                    onClick={() => {
+                      setSelectedRowData(entry);
+                      openViewExpenseModal();
+                    }}
+                  >
+                    <FontAwesomeIcon icon={faCircleInfo} />
+                  </button>
+                </td>
+              </tr>
+            ))}
+            {searchQuery.trim() !== "" && searchResults.length === 0 && (
+              <tr>
+                <td colSpan="4" className="text-center py-4">
+                  No results found.
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ExpenseTable;
