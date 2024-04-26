6f17ca9c4f19da863f2199f825782ba254661f5e
kkopoku <kk.opoku@outlook.com>
2024-04-26 21:14:42 +0000

    added prop-types to AuthenticatedLayout

--- components/layouts/authenticated_layout/authenticated_layout.js
+++ components/layouts/authenticated_layout/authenticated_layout.js
@@ -1,13 +1,12 @@
 import React from "react";
 import Navbar from "./navbar";
 import Sidebar from "./sidebar";
+import PropTypes from "prop-types";
 
 export default function AuthenticatedLayout({ children, username, organisation }) {
   return (
     <div className="flex min-h-screen bg-white">
-      <div className="">
-        <Sidebar />
-      </div>
+      <Sidebar />
       <div className="flex flex-col flex-grow"> {/* Use flex-grow to fill available space */}
         <Navbar username={username} organisation={organisation} />
         <div className="flex flex-col h-full">{children}</div>
@@ -15,3 +14,9 @@ export default function AuthenticatedLayout({ children, username, organisation }
     </div>
   );
 }
+
+AuthenticatedLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+  username: PropTypes.string.isRequired,
+  organisation: PropTypes.string.isRequired
+};
