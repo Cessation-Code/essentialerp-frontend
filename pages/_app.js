5fefab6ba6f49573efe87ed5a6672ce1e14d7487
kkopoku <kk.opoku@outlook.com>
2024-04-28 22:24:11 +0000

    removed unused code

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,10 @@
-// app.js
 import '../styles/globals.css';
 import 'typeface-poppins';
 import '@fortawesome/fontawesome-svg-core/styles.css';
-import { Provider } from "react-redux";
-import store from "../store/store";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider store={store}>
       <Component {...pageProps} />
-    </Provider>
   );
 }
 
